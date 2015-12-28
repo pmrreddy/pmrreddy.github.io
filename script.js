321b85b6506b4593e2d1b2d3b8862a059964852e
pmrreddy <pmrreddy@live.com>
2015-12-28 16:43:05 +0530

    Link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var app=angular.module('single-page-app',['ngRoute']);
+var app=angular.module('paramweb',['ngRoute']);
 app.config(function($routeProvider){
       $routeProvider
           .when('/',{
@@ -8,8 +8,7 @@ app.config(function($routeProvider){
                 templateUrl: 'about.html'
           });
 });
-app.controller('cfgController',function($scope){
-
+app.controller('hpController',function($scope){
     /*      
     Here you can handle controller for specific route as well.
     */
